81fa21cf4237b2e75d439c2b37283c807b6deab3
바키찬 <ckstmznf@naver.com>
2024-01-30 14:26:09 +0900

    MainPage 이미지 마우스 효과

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import {PageBasicStyle} from "../style/BasicStyle";
 import aiImage from "./../asset/novel_kaguya.jpg"
 import Button from "../component/Button";
-import {useRef} from "react";
+import {useRef, useState} from "react";
 import { useNavigate } from 'react-router-dom';
 
 const MainPageStyle = styled.div`
@@ -24,10 +24,7 @@ const MainPageStyle = styled.div`
   }
 
   .ai-image {
-    & img {
-      width: 100%;
-      border-radius: 4px;
-    }
+    
   }
 
   .info {
@@ -63,32 +60,59 @@ const MainPageStyle = styled.div`
   }
 `
 
-const AiImage = styled.div`
-  height: ${p => p.height}px;
+const AiImage = styled.img`
+  width: 100%;
+  border-radius: 4px;
   
-  background-image: url(${aiImage});
-  background-position: center;
-  background-size: cover;
-  
-  border-radius: 12px;
+  clip-path: ${p => {
+    const {x, y, isMouseIn} = p.mouse
+    if(isMouseIn){
+      return `circle(30% at ${x}px ${y}px)`
+    }
+    else {
+      return "none"
+    }
+  }};
 `
 
 const MainPage = () => {
   const infoElement = useRef()
   const navigate = useNavigate();
+  const [imageMouse, setImageMouse] = useState({
+    x: 0, y: 0, isMouseIn : false
+  })
+  //
+  // const onMouseOut = () => {
+  //   setImageMouse({...imageMouse, isMouseIn: false})
+  // }
+  // const onMouseMove = (e) => {
+  //   const {clientX, clientY, target} = e
+  //   // const x = target
+  //   setImageMouse({
+  //     x: clientX, y: clientY, isMouseIn: true
+  //   })
+  // }
 
   return (
     <MainPageStyle>
       <div className="content">
         <div className="ai-image">
-          <img src={aiImage} alt=""/>
+          <AiImage
+            src={aiImage} alt="AI 대표 이미지"
+            mouse={imageMouse}
+            // onMouseMove={onMouseMove}
+            // // onMouseEnter={onMouseEnter}
+            // onMouseOut={onMouseOut}
+          />
         </div>
         <div className="info">
           <div ref={infoElement}>
             <h2>소설 만드는 인공지능</h2>
             <h1>Novel Kaguya</h1>
             <p>1000개의 소설을 학습한 인공지능이 당신이 원하는 소설을 만들어줍니다.</p>
-            <Button onClick={() => navigate("/generate")}>사용해보기</Button>
+            <Button onClick={() => navigate("/generate", {
+              replace: false
+            })}>사용해보기</Button>
           </div>
         </div>
       </div>
